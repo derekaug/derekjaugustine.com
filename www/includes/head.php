afbc99afe4f9d0230b213ce8a69cd2c8f89003af
Derek J. Augustine <derek.augustine@archrival.com>
2014-05-11 11:27:38 -0500

    pinterest verification file, style changes and cachebreaker

--- www/includes/head.php
+++ www/includes/head.php
@@ -1,6 +1,7 @@
 <?php
 include_once(__DIR__ . '/../vendor_class/autoload.php');
-$main_css = '/css/main.' . Config::$asset_type . '.css';
+$main_cb = filemtime(Config::$root . '/css/main.' . Config::$asset_type . '.css');
+$main_css = '/css/main.' . Config::$asset_type . '.css?cb=' . $main_cb;
 ?>
 <link href="http://fonts.googleapis.com/css?family=Roboto:400,400italic,700,700italic%7CRusso+One%7CInconsolata" rel="stylesheet" type="text/css">
 <base href="<?php echo Config::$base_url; ?>" target="_self">
